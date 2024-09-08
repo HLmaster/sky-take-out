7c592aeff37d7f812ac55d58edbd9fb023e789b7
yvjia <hong_lin2005@163.com>
2024-09-08 09:36:47 +0800

    套餐分页查询业务

---
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -0,0 +1,34 @@
+package com.sky.controller.admin;
+
+import com.github.pagehelper.PageHelper;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/setmeal")
+@Slf4j
+public class SetmealController {
+
+    @Autowired
+    private SetmealService setmealService;
+
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO) {
+        PageResult result = setmealService.page(setmealPageQueryDTO);
+
+        return Result.success(result);
+    }
+}
