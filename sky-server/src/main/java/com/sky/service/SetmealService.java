bb4cfe7f052e1df55fbabfe6fabe7466b2079629
yvjia <hong_lin2005@163.com>
2024-09-10 07:46:05 +0800

    小程序端商店浏览业务

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -4,7 +4,11 @@ package com.sky.service;
 import com.sky.dto.SetmealDTO;
 import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.result.PageResult;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
+import com.sky.entity.Setmeal;
+
+import java.util.List;
 
 public interface SetmealService {
     /**
@@ -40,5 +44,23 @@ public interface SetmealService {
      */
     void updateStatus(Integer status, Integer id);
 
+    /**
+     * 更新套餐
+     * @param setmealDTO
+     */
     void update(SetmealDTO setmealDTO);
+
+    /**
+     * 条件查询
+     * @param setmeal
+     * @return
+     */
+    List<Setmeal> list(Setmeal setmeal);
+
+    /**
+     * 根据id查询菜品选项
+     * @param id
+     * @return
+     */
+    List<DishItemVO> getDishItemById(Long id);
 }
